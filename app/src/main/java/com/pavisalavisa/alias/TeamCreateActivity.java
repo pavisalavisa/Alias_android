40988b8629846040946c063d3adaaa713ef539a5
AntonioKristicevic <kristicevic.antonio@gmail.com>
2018-01-07 18:39:50 +0100

    Alternating background in teams table, minor bugfixes, font corrections.

--- app/src/main/java/com/pavisalavisa/alias/TeamCreateActivity.java
+++ app/src/main/java/com/pavisalavisa/alias/TeamCreateActivity.java
@@ -1,9 +1,10 @@
 package com.pavisalavisa.alias;
 
 import android.app.DialogFragment;
-import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -13,7 +14,7 @@ public class TeamCreateActivity extends AppCompatActivity {
     private Game game;
     DialogFragment dialog;
     TableLayout table;
-
+    static Boolean backgroundFlag=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +24,6 @@ public class TeamCreateActivity extends AppCompatActivity {
     }
 
     public void addNewTeam(View view){
-        //TODO: dodati funkcionalnost timovac
         popDialog();
     }
     private void popDialog(){
@@ -38,22 +38,42 @@ public class TeamCreateActivity extends AppCompatActivity {
         TableRow tr=new TableRow(this);
         addItemsToRow(tr,game.getLastAddedTeam());
         table.addView(tr);
+        setAlternatingBackground(tr);
     }
 
     private void addItemsToRow(TableRow tr, Team team){
-        //TODO: popravi font i izgled ovog sta se ubacuje
-        TextView teamName=new TextView(this);
-        teamName.setText(team.getTeamName());
-        tr.addView(teamName);
-
-        TextView playerOneName=new TextView(this);
-        playerOneName.setText(team.getPlayerOne());
-        tr.addView(playerOneName);
-
-        TextView playerTwoName=new TextView(this);
-        playerTwoName.setText(team.getPlayerTwo());
-        tr.addView(playerTwoName);
+        attachTextViewToRow(tr,team.getTeamName(),2);
+        attachTextViewToRow(tr,team.getPlayerOne(),1);
+        attachTextViewToRow(tr,team.getPlayerTwo(),1);
     }
+
+    private void attachTextViewToRow(TableRow tr,String text,int weight){
+        TextView myText=makeTextView(text,weight);
+        if(weight==1){
+            myText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        }
+        tr.addView(myText);
+
+    }
+
+    private TextView makeTextView(String text,int weight){
+        TextView teamName=new TextView(this);
+        teamName.setText(text);
+        teamName.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+        teamName.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT, (float)weight));
+        return teamName;
+    }
+
+    private void setAlternatingBackground(TableRow tableRow){
+        if(backgroundFlag) {
+                tableRow.setBackgroundColor(Color.parseColor("#add8e6"));
+            }
+        backgroundFlag=!backgroundFlag;
+
+
+    }
+
 
 
 }
